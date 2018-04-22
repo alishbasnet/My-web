0d616c5c97114a482c3f22f39f24f0601c5e052b
alishbasnet <basnet.alish12@gmail.com>
2018-04-23 00:36:32 +0545

    files uploaded

---
+++ year.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char name[10];
+    char lastname[10];
+    int old, real;
+    printf("\n Enter your first name and last name: ");
+    scanf("%c%c",&name,&lastname);
+    real=2017-old ;
+    printf("\n Hello %c %c . How are you? You were born in %d",name,lastname,real);
+    return 0;
+}
